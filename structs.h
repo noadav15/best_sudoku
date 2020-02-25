11aa53ad3c644f6ae71ad3df63144f86c82a0c1f
noa davidovitch <noadav96@gmail.com>
2020-02-25 17:21:43 +0200

    base

---
+++ structs.h
@@ -0,0 +1,23 @@
+/*
+ * structs.h
+ * this module contains all the structs that are needed for the game.
+ */
+
+#ifndef STRUCTS_H_
+#define STRUCTS_H_
+
+typedef struct{
+	int invalid;
+	int value;
+	int fixed;
+	} Cell;
+
+typedef struct{
+	int num_of_rows_in_block;
+	int num_of_colomns_in_block;
+	int board_size;
+	int marked;
+	Cell **board;
+	} Game;
+
+#endif /* STRUCTS_H_ */
