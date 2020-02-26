938228cfb1e2eea03a80f985b2089352c7d0e982
Tal <tal1403@gmail.com>
2020-02-26 19:27:02 +0200

    save

--- fileParsing.c
+++ fileParsing.c
@@ -20,20 +20,34 @@ int readFromFile(Game *game, char* fileDir){
 
 void saveToFile(Game *game, char* fileDir){
 	int i, j;
+	Cell cur;
 	char converted[12];
 	FILE *fptr = fopen(fileDir, "w");
+	if(fptr == NULL){
+				printf("ERROR: invalid file name or file doesn't exist\n");
+				return;
+	}
 	sprintf(converted, "%d", game->num_of_columns_in_block);
 	fprintf(fptr, converted);
-	if(fptr == NULL){
-			printf("ERROR: invalid file name or file doesn't exist\n");
-			return;
-	}
+	fprintf(fptr, " ");
+	sprintf(converted, "%d", game->num_of_rows_in_block);
+	fprintf(fptr, converted);
+	fprintf(fptr, "\n");
 	for(i=1;i<=game->board_size;i++){
 		for(j=1;j<=game->board_size;j++){
-			if((game->board)[i][j].value != 0){
-
+				cur = (game->board)[i][j];
+				sprintf(converted, "%d", cur.value);
+				fprintf(fptr, converted);
+				if(cur.fixed){
+					fprintf(fptr, ".");
+				}
+				if(j == game->board_size){
+					fprintf(fptr, "\n");
+				}
+				else{
+					fprintf(fptr, " ");
+				}
 			}
-		}
 	}
 	fclose(fptr);
 }
---
+++ fileParsing.h
@@ -0,0 +1,13 @@
+/*
+ * fileParsing.h
+ *
+ *  Created on: 26 ????? 2020
+ *      Author: Tal
+ */
+
+#ifndef FILEPARSING_H_
+#define FILEPARSING_H_
+void saveToFile(Game *game, char* fileDir);
+
+
+#endif /* FILEPARSING_H_ */
